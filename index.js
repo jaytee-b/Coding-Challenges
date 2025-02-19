eb69e3a6188e569b4aedf2dd47beb16086ee08ce
jaytee-b <jesutoniadeleye@gmail.com>
2025-02-19 22:27:35 +0100

    day3 comparison of arrays

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ function simpleArraySum(arr){
 
 
 //This function uses a for loop method to loop around each iterization till it gets to the end of the array (arr.length), then the loop stops.
+
+
+// Day 3. The task I had was to compare two arrays indexes and return points to arrays with greater values i.e if a[i] > b[i] return a = 1
+let c = 0
+let d = 0
+function compareArray(a,b) {
+    for (let i =0 ; i < a.length; i ++){
+        if(a[i] > b[i]){
+            c++
+        } else if (b[i] > a[i]) {
+            d++
+        }
+    }
+    
+    let combinedArray =Array.of(c,d)
+    
+    return combinedArray
+
+}
