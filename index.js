8af75a5fe9d707db840e7bd8a5484cd2a99678ca
jaytee-b <jesutoniadeleye@gmail.com>
2025-03-05 23:01:37 +0100

    Implemented function to find common multiples and factors between two arrays
    - Added logic to check for multiples of all elements in one array
    - Added logic to check for factors of all elements in another array
    - Optimized using loops for efficiency

--- index.js
+++ index.js
@@ -196,4 +196,32 @@ function kangaroo(x1, v1, x2,v2){
 }
 
 
-//Day 11.
+//Day 11. working with factors and multiples of numbers
+
+function getTotalX(a,b){
+  let count = 0;
+  let maxA = Math.max(...a);
+  let minB = Math.min(...b);
+  for (let i = maxA; i <= minB; i++){
+    let isValid = true
+
+    for (let j = 0; j < a.length; j++){
+      if(i % a[j] !== 0){
+        isValid = false
+      }
+    }
+
+    if(isValid){
+      for(j = 0; j < b.length; j++){
+        if(b[j] % i !== 0){
+          isValid = false
+
+        }
+      }
+    }
+    if(isValid){
+      count++
+    }
+  }
+  return count
+}
